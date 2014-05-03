db57945427bf39f19b8be156c8b38a14bf3d2cc0
Jose Luis Santos <jlsangom@gmail.com>
2014-05-03 10:37:52 +0200

    Test Inicial

---
+++ src/Tests/RomanNumeralsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\Tests;
+
+use src\RomanNumerals;
+
+class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInit()
+    {
+        $this->assertTrue(True);
+    }
+
+}
+
+
+
+
+
